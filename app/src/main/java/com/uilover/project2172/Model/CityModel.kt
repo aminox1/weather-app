1da10022ad0162e600b908c72a164c36ce86f4eb
aminox1 <aminemoumni61@gmail.com>
2025-01-30 12:31:21 +0100

    Initial commit: weather app

---
+++ app/src/main/java/com/uilover/project2172/Model/CityModel.kt
@@ -0,0 +1,11 @@
+package com.uilover.project2172.Model
+
+data class CityModel(
+    val cityName:String,
+    val temp:Int,
+    val picPath:String,
+    val wind:Int,
+    val humidity:Int,
+    val rain:Int
+
+)
---
+++ app/src/main/java/com/uilover/project2172/Model/HourlyModel.kt
@@ -0,0 +1,7 @@
+package com.uilover.project2172.Model
+
+data class HourlyModel(
+    val hour:String,
+    val temp:Int,
+    val picPath:String
+)
